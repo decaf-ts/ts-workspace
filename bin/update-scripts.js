192d17b7004933a9e58e0c6b8a92c4fc160d44b8
tvenceslau <tiago.venceslau@pdmfc.com>
2025-03-22 05:27:06 +0000

    setups auto update for scripts

--- bin/update-scripts.js
+++ bin/update-scripts.js
@@ -18,6 +18,20 @@ const scripts = [
   ".github/build.run.xml",
   ".github/build_prod.run.xml",
 
+  ".run/All Tests.run.xml",
+  ".run/build.run.xml",
+  ".run/build_prod.run.xml",
+  ".run/coverage.run.xml",
+  ".run/docs.run.xml",
+  ".run/drawings.run.xml",
+  ".run/flash-forward.run.xml",
+  ".run/Integration Tests.run.xml",
+  ".run/lint-fix.run.xml",
+  ".run/test_circular.run.xml",
+  ".run/uml.run.xml",
+  ".run/Unit Tests.run.xml",
+  ".run/update-scripts.run.xml",
+
   ".gitlab-ci.yml",
   ".prettierrc",
   ".eslint.config.js",
@@ -45,7 +59,6 @@ Promise.allSettled(scripts.map(async p => {
     console.log(`Downloaded ${p} to ${targetPath}`);
   });
   console.log("All scripts downloaded successfully.");
-  process.exit(0);
 }).catch(error => {
   console.error("Error overwriting files:", error);
   process.exit(1);
